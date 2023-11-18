6fdf1ee04a443e686117d9ebf9136fe683792f51
Olierook <robbert_olierook@hotmail.com>
2023-11-18 16:41:56 +0100

    tidy up

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -10,23 +10,6 @@ import settings from '../../content/_settings.json'
 
 export default function Footer() {
 	
-	const [gitHubInfo, setGitHubInfo] = useState({
-		stars: null,
-		forks: null,
-	});
-
-	useEffect(() => {
-		fetch( settings.portfolio.repo_api )
-			.then(response => response.json())
-			.then(json => {
-				const { stargazers_count, forks_count } = json;
-				setGitHubInfo({
-					stars: stargazers_count,
-					forks: forks_count,
-				});
-			})
-		.catch(e => console.error(e));
-	}, []);
 	
 	return (
 		<footer className={css.container}>
--- components/utils/icon.util.jsx
+++ components/utils/icon.util.jsx
@@ -1,18 +1,15 @@
 // Core packages
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 // Font Awesome packages
-const { library, config } = require('@fortawesome/fontawesome-svg-core')
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
-import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+const { library, config } = require("@fortawesome/fontawesome-svg-core");
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 // Load icons into
-library.add(fat, fal, fas, fad, far, fab)
+library.add(fas, far, fab);
 
 /**
  * Icon factory utility.
@@ -33,13 +30,13 @@ library.add(fat, fal, fas, fad, far, fab)
  */
 export default function Icon({ icon }) {
 
-	const [ iconType, iconKey ] = icon
+  const [ iconType, iconKey ] = icon;
 
-	const [ stateIconKey, setIconKey ] = useState('circle-notch')
+  const [ stateIconKey, setIconKey ] = useState("circle-notch");
 
-	useEffect( () => setIconKey( iconKey ), [ iconKey ] )
+  useEffect( () => setIconKey( iconKey ), [ iconKey ] );
 
-	return (
-		<FontAwesomeIcon icon={[ iconType, stateIconKey ]} />
-	)
+  return (
+    <FontAwesomeIcon icon={[ iconType, stateIconKey ]} />
+  );
 }
